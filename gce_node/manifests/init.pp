e01f3f263de3ad2a92960ebefaf2a6af410464e8
Frank Berghaus <frank@uvic.ca>
2014-04-29 11:53:31 -0700

    Absorb ganglia configuration into gce_node class
    Remove import of ganglia configuration to ensure the ganglia
    configuration is properly seperated for each experimts workers.
    
    Changes:
    	deleted:    gce_node/manifests/ganglia.pp
    	modified:   gce_node/manifests/init.pp

--- gce_node/manifests/init.pp
+++ gce_node/manifests/init.pp
@@ -141,8 +141,35 @@ class gce_node (
     class {'wlcg': }
 
     if $::gce_node::experiment_in == 'atlas' {
-      import 'ganglia.pp'
-    }
+      # ganglia manifest
+      #
+      # this sets up ganglia reporting to CERN's hn-grizzly. Need
+      # find a way to determine which cluster name which cloud should
+      # report to
+      $udp_recv_channel = [
+        { port => 9006 },
+      ]
 
+      $udp_send_channel = [
+        { port => 9006, host => 'hn-grizzly.cern.ch', ttl => 2 },
+      ]
+
+      $cluster_name = 'MULTI-CLUSTER_tmp'
+
+      $tcp_accept_channel = [
+        {port => 9006},
+      ]
+
+      class{ 'ganglia::gmond':
+        cluster_name       => $cluster_name,
+        cluster_owner      => 'unspecified',
+        cluster_latlong    => 'unspecified',
+        cluster_url        => 'unspecified',
+        host_location      => 'CERN',
+        udp_recv_channel   => $udp_recv_channel,
+        udp_send_channel   => $udp_send_channel,
+        tcp_accept_channel => $tcp_accept_channel,
+      }
+    }
   }
 }
