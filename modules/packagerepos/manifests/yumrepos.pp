98141fc1033b4d4b16e3bd398f187a1a19f98896
Frank Berghaus <frank@uvic.ca>
2014-07-15 15:01:01 -0700

    Remove operatingsystemmajorrelease from puppet
    Ensure modules work with CernVM by removing operatingsystemmajorrelease
    fact. Wait for puppet and facter to be updated on CernVM.
    
    Changes:
    	modified:   modules/packagerepos/manifests/yumrepos.pp

--- modules/packagerepos/manifests/yumrepos.pp
+++ modules/packagerepos/manifests/yumrepos.pp
@@ -8,34 +8,34 @@ class packagerepos::yumrepos {
 
   yumrepo {
     "slc6-extras":
-      descr    => "Scientific Linux CERN ${operatingsystemmajrelease} (SLC${operatingsystemmajrelease}) add-on packages",
-      baseurl  => "http://linuxsoft.cern.ch/cern/slc${operatingsystemmajrelease}X/${architecture}/yum/extras/",
+      descr    => "Scientific Linux CERN 6 (SLC6) add-on packages",
+      baseurl  => "http://linuxsoft.cern.ch/cern/slc6X/${architecture}/yum/extras/",
       gpgcheck => 0,
       enabled  => 1,
       protect  => 1;
     "slc6-rhcommon":
-      descr    => "Scientific Linux CERN (SLC${operatingsystemmajrelease}) - RHCOMMON addons",
-      baseurl  => "http://linuxsoft.cern.ch/cern/rhcommon/slc${operatingsystemmajrelease}X/${architecture}/yum/rhcommon/",
+      descr    => "Scientific Linux CERN (SLC6) - RHCOMMON addons",
+      baseurl  => "http://linuxsoft.cern.ch/cern/rhcommon/slc6X/${architecture}/yum/rhcommon/",
       gpgcheck => 0,
       enabled  => 1,
       protect  => 1;
     "epel":
-      descr      => "Extra Packages for Enterprise Linux ${operatingsystemmajrelease} - \$basearch",
-      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-${operatingsystemmajrelease}&arch=\$basearch",
+      descr      => "Extra Packages for Enterprise Linux 6 - \$basearch",
+      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=\$basearch",
       gpgcheck   => 0,
       enabled    => 1,
       priority   => 1,
       protect    => 1;
     "epel-testing":
-      descr      => "Extra Packages for Enterprise Linux ${operatingsystemmajrelease} - Testing - \$basearch",
-      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=testing-epel${operatingsystemmajrelease}&arch=$basearch",
+      descr      => "Extra Packages for Enterprise Linux 6 - Testing - \$basearch",
+      mirrorlist => "https://mirrors.fedoraproject.org/metalink?repo=testing-epel6&arch=$basearch",
       gpgcheck   => 0,
       enabled    => 1,
       failovermethod => 'priority',
       protect    => 1;
     "cvmfs":
       descr    => "CernVM packages",
-      baseurl  => "http://cern.ch/cvmrepo/yum/cvmfs/EL/${operatingsystemmajrelease}/${architecture}",
+      baseurl  => "http://cern.ch/cvmrepo/yum/cvmfs/EL/6/${architecture}",
       gpgcheck => 1,
       gpgkey   => 'https://cvmrepo.web.cern.ch/cvmrepo/yum/RPM-GPG-KEY-CernVM',
       enabled  => 1,
@@ -43,15 +43,15 @@ class packagerepos::yumrepos {
       protect  => 1;
     "cvmfs-testing":
       descr    => "CernVM testing packages",
-      baseurl  => "http://cern.ch/cvmrepo/yum/cvmfs-testing/EL/${operatingsystemmajrelease}/${architecture}",
+      baseurl  => "http://cern.ch/cvmrepo/yum/cvmfs-testing/EL/6/${architecture}",
       gpgcheck => 1,
       gpgkey   => 'https://cvmrepo.web.cern.ch/cvmrepo/yum/RPM-GPG-KEY-CernVM',
       enabled  => 1,
       priority => 99,
       protect  => 1;
     "htcondor":
-      descr => "HTCondor Stable RPM Repository for Redhat Enterprise Linux ${operatingsystemmajrelease}",
-      baseurl => "http://research.cs.wisc.edu/htcondor/yum/stable/rhel${operatingsystemmajrelease}",
+      descr => "HTCondor Stable RPM Repository for Redhat Enterprise Linux 6",
+      baseurl => "http://research.cs.wisc.edu/htcondor/yum/stable/rhel6",
       enabled => 1,
       gpgcheck => 0,
       priority => 1,
