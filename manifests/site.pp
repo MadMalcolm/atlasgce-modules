70b2162fd1f8132c3eeca49a5420c534d6b65e6f
Frank Berghaus <frank@uvic.ca>
2014-07-28 15:38:26 -0700

    Revert to default only in site manifest
    Use default node only in site manifest. Nodes specified by cert name did
    not run in test.
    
    Changes:
    	modified:   manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,7 @@
 ################################################################################
-node atlas-worker.heprc.uvic.ca.pem {
-  
-  class { 'gce_node::atlas':
-    cvmfs_cache    => '/var/cache/cvmfs2',
-    condor_homedir => '/var/lib/condor',
-  }
-  
-}
-
-
-################################################################################
-node belle-worker.heprc.uvic.ca.pem {
-  
-  class { 'gce_node::belle':
-    cvmfs_cache    => '/var/cache/cvmfs2',
-    condor_homedir => '/var/lib/condor',
-  }
-  
-}
-
-
-################################################################################
 node default {
   
-  class { 'gce_node::atlas':
-    cvmfs_cache    => '/var/cache/cvmfs2',
-    condor_homedir => '/var/lib/condor',
+  class { 'gce_node':
   }
   
 }
