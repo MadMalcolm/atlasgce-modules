71b3fc1019728520550528930ed7d2ba642e939c
Frank Berghaus <frank@uvic.ca>
2014-07-28 12:42:42 -0700

    Inset configuration for atlas worker node as well as default
    Add atlas worker specific configuration to site manifest even though for
    now it is the default.
    
    Changes:
    	modified:   manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,16 @@
 ################################################################################
-node default {
+node atlas-worker.heprc.uvic.ca.pem {
+  
+  class { 'gce_node::atlas':
+    cvmfs_cache    => '/var/cache/cvmfs2',
+    condor_homedir => '/var/lib/condor',
+  }
+  
+}
 
+
+################################################################################
+node default {
   
   class { 'gce_node::atlas':
     cvmfs_cache    => '/var/cache/cvmfs2',
