44d57a3b86172450cdd909774d3481472a956d31
Frank Berghaus <frank@uvic.ca>
2014-08-20 13:40:38 -0700

    Remove default node from dev environment
    Reserve default node for production environment in puppet. Puppet does
    not allow default for nodes in other environments.
    
    Changes:
    	modified:   manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,9 @@
 ################################################################################
-node default {
+node 'worker.heprc.uvic.ca' {
   
-  class { 'gce_node':
+  class { 'gce_node::atlas':
+    cvmfs_cache    => '/var/cache/cvmfs2',
+    condor_homedir => '/var/lib/condor',
   }
   
 }
