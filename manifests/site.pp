23d60b9ccb47f7bda672640d6de66d8c680833a1
Frank Berghaus <frank@uvic.ca>
2014-09-03 09:43:40 -0700

    Specify belle configuration in site manifest
    Call belle modules in the site manifest to customize this branch for belle
    configuration.
    
    Changes:
    	modified:   manifests/site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 ################################################################################
 node 'worker.heprc.uvic.ca' {
   
-  class { 'gce_node::atlas':
+  class { 'gce_node::belle':
     cvmfs_cache    => '/var/cache/cvmfs2',
     condor_homedir => '/var/lib/condor',
   }
